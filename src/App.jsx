ece5cb4f3da5fbcbf0243989baf8c46b6c29e7ce
eSTee3 <tagsxa@yahoo.com>
2023-03-24 20:06:42 -0500

    Relocated several files so they actually work!

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+// import Header from './components/header/Header'
+
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Project from './components/project/Project';
+
+const App = () => {
+  return (
+    <>
+      <About />
+      <Contact />
+      <Footer />
+      <Header />
+      <Nav />
+      <Project />
+    </>
+  )
+}
+
+export default App
